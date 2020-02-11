cddada982c70214584bf3715d74f148d737860ab
yanwenhao <ywh_687@163.com>
2020-02-11 21:19:38 +0800

    Initial commit

---
+++ src/com/yanwenhao/bean/HelloTest.java
@@ -0,0 +1,26 @@
+package com.yanwenhao.bean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author YANWENHAO
+ * @create 2020-02-11 20:05
+ */
+public class HelloTest {
+
+//    psf:全局常量      prsf:私有的静态常量
+    public static final String s = "abcd";
+    private static final Integer i = 1;
+
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        System.out.println("list = " + list);
+        int i = 1;
+        System.out.println(i);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dell
+  Date: 2020/2/11
+  Time: 19:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>第一个web工程</title>
+  </head>
+  <body>
+  <h2>Hello World</h2>
+  <h2>Hi</h2>
+  </body>
+</html>
